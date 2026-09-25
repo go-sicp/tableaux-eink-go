1188e8f97398da9078a7a37da134bf6f5397843e
tannevaled <tannevaled@users.noreply.github.com>
2026-09-25 17:54:10 +0200

    fix(deps): update dependency androidx.core:core-ktx to v1.19.1 (#5)

--- android-host/app/build.gradle.kts
+++ android-host/app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.core:core-ktx:1.19.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
 
